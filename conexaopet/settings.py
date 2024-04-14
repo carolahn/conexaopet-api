6d8243e87f58ea0cf01da163be9911320daa21a7
carolahn <ahncarolina@gmail.com>
2024-04-14 10:11:44 -0300

    chore: github workflow 2

--- conexaopet/settings.py
+++ conexaopet/settings.py
@@ -172,8 +172,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  # Porta TLS
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default=config('EMAIL_HOST_USER'))
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default=config('EMAIL_HOST_PASSWORD'))
 
 # URL das imagens
 CUPOM_IMAGES_DIR = os.path.join(BASE_DIR, 'media', 'cupom_images')
